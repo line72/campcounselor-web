7745538a4635525a9249276a7912390ebc3bbc25
Marcus Dillavou <line72@line72.net>
2025-10-02 07:55:09 -0500

    Fixed service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -40,18 +40,3 @@ self.addEventListener('fetch', event => {
       });
   );
 });
-
-// Activate event - clean up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
